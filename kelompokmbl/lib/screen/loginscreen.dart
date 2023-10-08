81c0f3299c1e6018a4e1459d78735230ffc277ad
Jessica Hutagalung <jessicauly01@gmail.com>
2023-10-08 10:48:49 +0700

    shared_preferences

--- kelompokmbl/lib/screen/loginscreen.dart
+++ kelompokmbl/lib/screen/loginscreen.dart
@@ -1,5 +1,11 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
-import 'package:kelompokmbl/data/color.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:kelompokmbl/provider/login_sharedpreferences.dart';
+import 'package:kelompokmbl/screen/homescreen.dart';
+import 'package:kelompokmbl/screen/registerscreen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:kelompokmbl/screen/navbarscreen.dart';
 
 class ScreenLogin extends StatefulWidget {
@@ -10,109 +16,169 @@ class ScreenLogin extends StatefulWidget {
 }
 
 class _ScreenLoginState extends State<ScreenLogin> {
-  final TextEditingController _inputEmail = TextEditingController();
-  final TextEditingController _inputPassword = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  late SharedPreferences loginData;
+  late bool newUser;
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.all(15.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 80,
-              ),
-              // Center(
-              //   child: Container(
-              //     child: Image.asset('assets/ilustration-login.png'),
-              //   ),
-              // ),
-              Center(
-                child: Container(
-                  child: Image.asset('assets/loginscrenn.jpg'),
-                ),
-              ),
-              Container(
-                  margin: EdgeInsets.only(top: 10),
-                  child: Text(
-                    'Login',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                  )),
-              Container(
-                margin: EdgeInsets.only(top: 20),
-                child: TextField(
-                  controller: _inputEmail,
-                  decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      borderSide: BorderSide(width: 2, color: Colors.black),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      borderSide: BorderSide(width: 2, color: Colors.black),
-                    ),
-                    labelText: 'Email',
+          padding: const EdgeInsets.symmetric(vertical: 80.0, horizontal: 15.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 25),
+                  child: Center(
+                    child: Image.asset('assets/loginscrenn.jpg'),
                   ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 20),
-                child: TextField(
-                  controller: _inputEmail,
-                  obscureText: mounted,
-                  decoration: InputDecoration(  
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      borderSide: BorderSide(width: 2, color: Colors.black),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      borderSide: BorderSide(width: 2, color: Colors.black),
-                    ),
-                    labelText: 'Password',
-                  ),
-                ),
-              ),
-              Container(
-                  child: ListTile(
-                leading: Text('Remember me'),
-                trailing: Text(
-                  'Needs Help',
-                  style: TextStyle(
-                      color: MyColor.primaryColor, fontWeight: FontWeight.bold),
-                ),
-              )),
-              Container(
-                child: Container(
-                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-                    width: double.infinity,
-                    height: 60,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => NavbarScreen()));
-                      },
-                      child: Text(
-                        'Login',
-                        style: TextStyle(fontSize: 18, color: Colors.black),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: TextFormField(
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: const BorderSide(width: 2, color: Colors.black),
                       ),
-                      style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          primary: MyColor.primaryColor),
-                    )),
-              ),
-              Align(
-                  alignment: Alignment.center,
-                  child: Container(child: Text('Create An Account?')))
-            ],
-          ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: const BorderSide(width: 2, color: Colors.black),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: const BorderSide(width: 2, color: Colors.black),
+                      ),
+                      prefixIcon: const Icon(Icons.person),
+                      labelText: 'Username',
+                      labelStyle: GoogleFonts.lexendDeca()
+                    ),
+                    validator: (value) {
+                      if (value != null && value.length < 5) {
+                        return "Enter at least 5 characters";
+                      }
+                      return null;
+                    },
+                    keyboardType: TextInputType.name,
+                    style: GoogleFonts.lexendDeca(),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: TextFormField(
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    controller: _passwordController,
+                    obscureText: true,
+                    obscuringCharacter: "*",
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: const BorderSide(width: 2, color: Colors.black),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: const BorderSide(width: 2, color: Colors.black),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: const BorderSide(width: 2, color: Colors.black),
+                      ),
+                      prefixIcon: const Icon(Icons.lock),
+                      labelText: 'Password',
+                      labelStyle: GoogleFonts.lexendDeca()
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please input your password";
+                      } else if (value.length < 8) {
+                        return "Enter min. 8 characters";
+                      }
+                      return null;
+                    },
+                    keyboardType: TextInputType.visiblePassword,
+                    style: GoogleFonts.lexendDeca(),
+                  ),
+                ),
+                const SizedBox(height: 15.0),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                  width: double.infinity,
+                  height: 60,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      _logIn();
+                    },
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        primary: const Color(0xCC3BE2EF)),
+                    child: Text('Login',
+                        style: GoogleFonts.lexendDeca(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                            color: Colors.black)),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Dont have any account?',
+                        style: GoogleFonts.lexendDeca(
+                            fontSize: 12, color: Colors.black)),
+                    const SizedBox(width: 5.0),
+                    InkWell(
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => const RegisterScreen()));
+                      },
+                      child: Text('Register now',
+                          style: GoogleFonts.lexendDeca(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 12,
+                              color: Colors.blue)),
+                    ),
+                  ],
+                ),
+              ],
+            )
+          )
         ),
       ),
     );
+  }
+
+  _logIn() async {
+    final isValidForm = _formKey.currentState!.validate();
+    String username = _nameController.text;
+
+    await LoginSharedPreferences.saveUserLogInPreference(true);
+    await LoginSharedPreferences.saveUserNamePreference(username);
+
+    if (isValidForm) {
+      Navigator.pushAndRemoveUntil(
+        context, 
+        MaterialPageRoute(
+          builder: (_) => const HomeScreen(),
+        ), 
+        (route) => false);
+    }
   }
 }
