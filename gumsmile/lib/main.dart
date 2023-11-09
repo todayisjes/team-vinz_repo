479b85ac955261d5d1c8e21180492e3c54c6e3e7
Jessica Hutagalung <jessicauly01@gmail.com>
2023-11-10 02:21:03 +0700

    update main()

--- gumsmile/lib/main.dart
+++ gumsmile/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:gumsmile/const.dart';
@@ -12,7 +13,9 @@ import 'package:gumsmile/provider/past_provider.dart';
 import 'package:gumsmile/view/screen/navbarscreen.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
