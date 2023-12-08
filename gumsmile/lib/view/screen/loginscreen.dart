228894c148b729a47674f2bfa62749f1f0679c1b
Jessica Hutagalung <jessicauly01@gmail.com>
2023-12-08 19:57:00 +0700

    update login

--- gumsmile/lib/view/screen/loginscreen.dart
+++ gumsmile/lib/view/screen/loginscreen.dart
@@ -1,10 +1,9 @@
-// ignore_for_file: use_build_context_synchronously
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:gumsmile/authentication.dart';
+import 'package:gumsmile/provider/login_provider.dart';
 import 'package:gumsmile/view/screen/navbarscreen.dart';
 import 'package:gumsmile/view/screen/registerscreen.dart';
+import 'package:provider/provider.dart';
 
 class ScreenLogin extends StatefulWidget {
   const ScreenLogin({super.key});
@@ -14,175 +13,175 @@ class ScreenLogin extends StatefulWidget {
 }
 
 class _ScreenLoginState extends State<ScreenLogin> {
-  final _formKey = GlobalKey<FormState>();
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
+  final LoginProvider _loginProvider = LoginProvider();
 
   String? email;
   String? password;
 
   @override
   void dispose() {
-    _emailController.dispose();
-    _passwordController.dispose();
+    _loginProvider.emailController.dispose();
+    _loginProvider.passwordController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    final prov = Provider.of<LoginProvider>(context, listen: false);
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 80.0, horizontal: 15.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 25),
-                  child: Center(
-                    child: Image.asset('assets/loginscrenn.jpg'),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 10.0),
-                  child: TextFormField(
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    controller: _emailController,
-                    decoration: InputDecoration(
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide:
-                              const BorderSide(width: 2, color: Colors.black),
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide:
-                              const BorderSide(width: 2, color: Colors.black),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide:
-                              const BorderSide(width: 2, color: Colors.black),
-                        ),
-                        prefixIcon: const Icon(Icons.email),
-                        labelText: 'Email',
-                        hintText: 'jessie@gmail.com',
-                        labelStyle: GoogleFonts.lexendDeca()),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "Please enter your email address";
-                      } else if (!value.contains("@") || !value.contains(".")) {
-                        return "Please enter a valid email address";
-                      }
-                      return null;
-                    },
-                    onSaved: (val) {
-                      email = val;
-                    },
-                    keyboardType: TextInputType.emailAddress,
-                    style: GoogleFonts.lexendDeca(),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 10.0),
-                  child: TextFormField(
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    controller: _passwordController,
-                    obscureText: true,
-                    obscuringCharacter: "*",
-                    decoration: InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                        borderSide: const BorderSide(width: 2, color: Colors.black),
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                        borderSide: const BorderSide(width: 2, color: Colors.black),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                        borderSide: const BorderSide(width: 2, color: Colors.black),
-                      ),
-                      prefixIcon: const Icon(Icons.lock),
-                      labelText: 'Password',
-                      labelStyle: GoogleFonts.lexendDeca()
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "Please input your password";
-                      } else if (value.length < 8) {
-                        return "Enter min. 8 characters";
-                      }
-                      return null;
-                    },
-                    onSaved: (val) {
-                      password = val;
-                    },
-                    keyboardType: TextInputType.visiblePassword,
-                    style: GoogleFonts.lexendDeca(),
-                  ),
-                ),
-                const SizedBox(height: 15.0),
-                Container(
-                  padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-                  width: double.infinity,
-                  height: 60,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        _formKey.currentState!.save();
-                        AuthenticationHelper().logIn(
-                          email: email!, 
-                          password: password!
-                        ).then((result) {
-                          if (result == null) {
-                            Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const NavbarScreen()));
-                          } else {
-                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(result, style: GoogleFonts.lexendDeca(fontSize: 16))));
-                          }
-                        });
-                      }
-                    },
-                    style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        primary: const Color(0xCC3BE2EF)),
-                    child: Text('Login',
-                        style: GoogleFonts.lexendDeca(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                            color: Colors.black)),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('Dont have any account?',
-                        style: GoogleFonts.lexendDeca(
-                            fontSize: 12, color: Colors.black)),
-                    const SizedBox(width: 5.0),
-                    InkWell(
-                      onTap: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => const RegisterScreen()));
-                      },
-                      child: Text('Register now',
-                          style: GoogleFonts.lexendDeca(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 12,
-                              color: Colors.blue)),
-                    ),
-                  ],
-                ),
-              ],
+      body: prov.isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
             )
-          )
-        ),
-      ),
+          : SingleChildScrollView(
+              child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 80.0, horizontal: 15.0),
+                  child: Form(
+                      key: prov.formKey,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 25),
+                            child: Center(
+                              child: Image.asset('assets/loginscrenn.jpg'),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10.0),
+                            child: TextFormField(
+                              autovalidateMode:
+                                  AutovalidateMode.onUserInteraction,
+                              controller: prov.emailController,
+                              decoration: InputDecoration(
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(20),
+                                    borderSide: const BorderSide(
+                                        width: 2, color: Colors.black),
+                                  ),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(20),
+                                    borderSide: const BorderSide(
+                                        width: 2, color: Colors.black),
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(20),
+                                    borderSide: const BorderSide(
+                                        width: 2, color: Colors.black),
+                                  ),
+                                  prefixIcon: const Icon(Icons.email),
+                                  labelText: 'Email',
+                                  hintText: 'johndoe@gmail.com',
+                                  labelStyle: GoogleFonts.lexendDeca()),
+                              validator: (value) => prov.validatorEmail(value),
+                              onSaved: (val) {
+                                email = val;
+                              },
+                              keyboardType: TextInputType.emailAddress,
+                              style: GoogleFonts.lexendDeca(),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10.0),
+                            child: TextFormField(
+                              autovalidateMode:
+                                  AutovalidateMode.onUserInteraction,
+                              controller: prov.passwordController,
+                              obscureText: true,
+                              obscuringCharacter: "*",
+                              decoration: InputDecoration(
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(20),
+                                    borderSide: const BorderSide(
+                                        width: 2, color: Colors.black),
+                                  ),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(20),
+                                    borderSide: const BorderSide(
+                                        width: 2, color: Colors.black),
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(20),
+                                    borderSide: const BorderSide(
+                                        width: 2, color: Colors.black),
+                                  ),
+                                  prefixIcon: const Icon(Icons.lock),
+                                  labelText: 'Password',
+                                  labelStyle: GoogleFonts.lexendDeca()),
+                              validator: (value) => prov.validatorPassword(value),
+                              onSaved: (val) {
+                                password = val;
+                              },
+                              keyboardType: TextInputType.visiblePassword,
+                              style: GoogleFonts.lexendDeca(),
+                            ),
+                          ),
+                          const SizedBox(height: 15.0),
+                          Container(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 30),
+                            width: double.infinity,
+                            height: 60,
+                            child: Consumer<LoginProvider>(builder: (context, regProv, child) {
+                              return ElevatedButton(
+                                onPressed: () {
+                                  if (regProv.formKey.currentState!.validate()) {
+                                    regProv.checkLogin(regProv.emailController.text, regProv.passwordController.text).then((value) {
+                                      if (value) {
+                                        var snackBar = SnackBar(
+                                          content: Text('Success to Login', style: GoogleFonts.lexendDeca(fontSize: 16.0),),
+                                        );
+                                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(builder: (context) => const NavbarScreen())
+                                        );
+                                      } else {
+                                        var snackBar = SnackBar(content: Text('Failed to Login', style: GoogleFonts.lexendDeca(fontSize: 16.0),));
+                                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                                      }
+                                    });
+                                  }
+                                },
+                                style: ElevatedButton.styleFrom(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20),
+                                    ),
+                                    primary: const Color(0xCC3BE2EF)),
+                                child: Text('Login',
+                                    style: GoogleFonts.lexendDeca(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18,
+                                        color: Colors.black)),
+                              );
+                            }),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text('Dont have any account?',
+                                  style: GoogleFonts.lexendDeca(
+                                      fontSize: 12, color: Colors.black)),
+                              const SizedBox(width: 5.0),
+                              InkWell(
+                                onTap: () {
+                                  Navigator.of(context).push(MaterialPageRoute(
+                                      builder: (context) =>
+                                          const RegisterScreen()));
+                                },
+                                child: Text('Register now',
+                                    style: GoogleFonts.lexendDeca(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 12,
+                                        color: Colors.blue)),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ))),
+            ),
     );
   }
 }
