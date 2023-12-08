6e52dd3d2b5a514f628b236aab8b6cb05486c70c
Jessica Hutagalung <jessicauly01@gmail.com>
2023-12-08 19:26:30 +0700

    update

--- gumsmile/lib/view/screen/welcomescreen.dart
+++ gumsmile/lib/view/screen/welcomescreen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:gumsmile/view/screen/loginscreen.dart';
-import 'package:gumsmile/view/screen/navbarscreen.dart';
 import 'package:gumsmile/view/screen/registerscreen.dart';
 
 class WelcomeScreen extends StatelessWidget {
