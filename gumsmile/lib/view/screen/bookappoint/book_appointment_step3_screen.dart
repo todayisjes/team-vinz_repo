f2d9af36c1a166842e53a7de399d9b13ebfe66a3
Jessica Hutagalung <jessicauly01@gmail.com>
2023-12-26 20:31:40 +0700

    update step3 screen

--- gumsmile/lib/view/screen/bookappoint/book_appointment_step3_screen.dart
+++ gumsmile/lib/view/screen/bookappoint/book_appointment_step3_screen.dart
@@ -1,185 +1,193 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:gumsmile/const.dart';
 import 'package:gumsmile/data/data.dart';
+import 'package:gumsmile/provider/book_apointment_provider.dart';
 import 'package:gumsmile/view/screen/listappointmentscreen.dart';
 import 'package:gumsmile/view/screen/promoscreen.dart';
+import 'package:provider/provider.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 
 class BookAppointmentStep3Screen extends StatefulWidget {
   const BookAppointmentStep3Screen({super.key});
 
   @override
-  State<BookAppointmentStep3Screen> createState() => _BookAppointmentStep3ScreenState();
+  State<BookAppointmentStep3Screen> createState() =>
+      _BookAppointmentStep3ScreenState();
 }
 
 enum PaymentMethod { card, cod, ovo, dana, spay, gopay }
 
 class _BookAppointmentStep3ScreenState extends State<BookAppointmentStep3Screen> {
   PaymentMethod? _paymentItem = PaymentMethod.card;
+  
+  late InterstitialAd _interstitialAd;
+  bool _isInterstitialReady = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'STEP 3 OUT OF 3', 
-          style: Constant().textAppBar
+        appBar: AppBar(
+          title: Text('STEP 3 OUT OF 3', style: Constant().textAppBar), 
+          centerTitle: true,
+          elevation: 0,
+          backgroundColor: kPrimaryColor,
         ),
-        centerTitle: true,
-        elevation: 0,
-        backgroundColor: kPrimaryColor,
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          width: double.infinity,
-          height: 800,
-          decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [Color.fromARGB(225, 239, 254, 254), Colors.white])),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 70, bottom: 10),
-                child: Center(
-                    child: Text('Total Cost',
-                        style: GoogleFonts.lexendDeca(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black))),
+        body: Consumer<BookAppointmentProvider>(builder: (context, bookProv, _) {
+          return SingleChildScrollView(
+            child: Container(
+              width: double.infinity,
+              height: 800,
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                    Color.fromARGB(225, 239, 254, 254),
+                    Colors.white
+                  ])),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 70, bottom: 10),
+                    child: Center(
+                        child: Text('Total Cost',
+                            style: GoogleFonts.lexendDeca(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black))),
+                  ),
+                  Center(
+                      child: Text('IDR ${bookProv.selectedTreatment!.price}',
+                          style: GoogleFonts.libreBaskerville(
+                              fontSize: 50, color: const Color(0xFF00DEE4)))),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 20.0),
+                    child: Center(
+                        child: InkWell(
+                            onTap: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => const PromoScreen()));
+                            },
+                            child: Text(
+                              'Voucher in here!',
+                              style: GoogleFonts.notoSerif(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500,
+                                  color: const Color(0xFFEB1F1F),
+                                  decoration: TextDecoration.underline),
+                            ))),
+                  ),
+                  Center(
+                      child: Text('Choose Your Payment Method',
+                          style: GoogleFonts.lexendDeca(
+                              fontSize: 20, color: Color(0xFF676767)))),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30.0, left: 40.0),
+                    child: Column(
+                      children: [
+                        RadioListTile<PaymentMethod>(
+                          value: PaymentMethod.card,
+                          groupValue: _paymentItem,
+                          onChanged: (PaymentMethod? value) {
+                            setState(() {
+                              _paymentItem = value;
+                            });
+                          },
+                          title: Text('Credit Card/Debit Card',
+                              style: GoogleFonts.lexendDeca(
+                                  fontSize: 12, color: Colors.black)),
+                        ),
+                        RadioListTile<PaymentMethod>(
+                          value: PaymentMethod.cod,
+                          groupValue: _paymentItem,
+                          onChanged: (PaymentMethod? value) {
+                            setState(() {
+                              _paymentItem = value;
+                            });
+                          },
+                          title: Text('Cash On Delivey',
+                              style: GoogleFonts.lexendDeca(
+                                  fontSize: 12, color: Colors.black)),
+                        ),
+                        RadioListTile<PaymentMethod>(
+                          value: PaymentMethod.ovo,
+                          groupValue: _paymentItem,
+                          onChanged: (PaymentMethod? value) {
+                            setState(() {
+                              _paymentItem = value;
+                            });
+                          },
+                          title: Text('OVO',
+                              style: GoogleFonts.lexendDeca(
+                                  fontSize: 12, color: Colors.black)),
+                        ),
+                        RadioListTile<PaymentMethod>(
+                          value: PaymentMethod.dana,
+                          groupValue: _paymentItem,
+                          onChanged: (PaymentMethod? value) {
+                            setState(() {
+                              _paymentItem = value;
+                            });
+                          },
+                          title: Text('DANA',
+                              style: GoogleFonts.lexendDeca(
+                                  fontSize: 12, color: Colors.black)),
+                        ),
+                        RadioListTile<PaymentMethod>(
+                          value: PaymentMethod.spay,
+                          groupValue: _paymentItem,
+                          onChanged: (PaymentMethod? value) {
+                            setState(() {
+                              _paymentItem = value;
+                            });
+                          },
+                          title: Text('ShopeePay',
+                              style: GoogleFonts.lexendDeca(
+                                  fontSize: 12, color: Colors.black)),
+                        ),
+                        RadioListTile<PaymentMethod>(
+                          value: PaymentMethod.gopay,
+                          groupValue: _paymentItem,
+                          onChanged: (PaymentMethod? value) {
+                            setState(() {
+                              _paymentItem = value;
+                            });
+                          },
+                          title: Text('GoPay',
+                              style: GoogleFonts.lexendDeca(
+                                  fontSize: 12, color: Colors.black)),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Center(
+                    child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 40),
+                        child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: const Color(0xFFB3F7F9),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(100)),
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 150),
+                                elevation: 0),
+                            onPressed: () => _onAlertDialogSuccess(context),
+                            child: Text(
+                              'Pay',
+                              style: GoogleFonts.lexendDeca(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w700,
+                                  color: Color(0xFF454545)),
+                            ))),
+                  )
+                ],
               ),
-              Center(
-                  child: Text('IDR 100000',
-                      style: GoogleFonts.libreBaskerville(
-                          fontSize: 50, color: const Color(0xFF00DEE4)))),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20.0),
-                child: Center(
-                    child: InkWell(
-                        onTap: () {
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => const PromoScreen()));
-                        },
-                        child: Text(
-                          'Voucher in here!',
-                          style: GoogleFonts.notoSerif(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w500,
-                              color: const Color(0xFFEB1F1F),
-                              decoration: TextDecoration.underline),
-                        ))),
-              ),
-              Center(
-                  child: Text('Choose Your Payment Method',
-                      style: GoogleFonts.lexendDeca(
-                          fontSize: 20, color: Color(0xFF676767)))),
-              Padding(
-                padding: const EdgeInsets.only(top: 30.0, left: 40.0),
-                child: Column(
-                  children: [
-                    RadioListTile<PaymentMethod>(
-                      value: PaymentMethod.card,
-                      groupValue: _paymentItem,
-                      onChanged: (PaymentMethod? value) {
-                        setState(() {
-                          _paymentItem = value;
-                        });
-                      },
-                      title: Text('Credit Card/Debit Card',
-                          style: GoogleFonts.lexendDeca(
-                              fontSize: 12, color: Colors.black)),
-                    ),
-                    RadioListTile<PaymentMethod>(
-                      value: PaymentMethod.cod,
-                      groupValue: _paymentItem,
-                      onChanged: (PaymentMethod? value) {
-                        setState(() {
-                          _paymentItem = value;
-                        });
-                      },
-                      title: Text('Cash On Delivey',
-                          style: GoogleFonts.lexendDeca(
-                              fontSize: 12, color: Colors.black)),
-                    ),
-                    RadioListTile<PaymentMethod>(
-                      value: PaymentMethod.ovo,
-                      groupValue: _paymentItem,
-                      onChanged: (PaymentMethod? value) {
-                        setState(() {
-                          _paymentItem = value;
-                        });
-                      },
-                      title: Text('OVO',
-                          style: GoogleFonts.lexendDeca(
-                              fontSize: 12, color: Colors.black)),
-                    ),
-                    RadioListTile<PaymentMethod>(
-                      value: PaymentMethod.dana,
-                      groupValue: _paymentItem,
-                      onChanged: (PaymentMethod? value) {
-                        setState(() {
-                          _paymentItem = value;
-                        });
-                      },
-                      title: Text('DANA',
-                          style: GoogleFonts.lexendDeca(
-                              fontSize: 12, color: Colors.black)),
-                    ),
-                    RadioListTile<PaymentMethod>(
-                      value: PaymentMethod.spay,
-                      groupValue: _paymentItem,
-                      onChanged: (PaymentMethod? value) {
-                        setState(() {
-                          _paymentItem = value;
-                        });
-                      },
-                      title: Text('ShopeePay',
-                          style: GoogleFonts.lexendDeca(
-                              fontSize: 12, color: Colors.black)),
-                    ),
-                    RadioListTile<PaymentMethod>(
-                      value: PaymentMethod.gopay,
-                      groupValue: _paymentItem,
-                      onChanged: (PaymentMethod? value) {
-                        setState(() {
-                          _paymentItem = value;
-                        });
-                      },
-                      title: Text('GoPay',
-                          style: GoogleFonts.lexendDeca(
-                              fontSize: 12, color: Colors.black)),
-                    ),
-                  ],
-                ),
-              ),
-              Center(
-                child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 40),
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: const Color(0xFFB3F7F9),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(100)),
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: 150),
-                            elevation: 0),
-                        onPressed: () => _onAlertDialogSuccess(context),
-                        child: Text(
-                          'Pay',
-                          style: GoogleFonts.lexendDeca(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w700,
-                              color: Color(0xFF454545)),
-                        ))),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
+        }));
   }
 
   _onAlertDialogSuccess(BuildContext context) {
@@ -212,6 +220,10 @@ class _BookAppointmentStep3ScreenState extends State<BookAppointmentStep3Screen>
         buttons: [
           DialogButton(
               onPressed: () {
+                _loadInterstitialAd();
+                if (_isInterstitialReady) {
+                  _interstitialAd.show();
+                }
                 Navigator.of(context).push(MaterialPageRoute(
                     builder: (context) => const ListAppointmentScreen()));
               },
@@ -223,5 +235,28 @@ class _BookAppointmentStep3ScreenState extends State<BookAppointmentStep3Screen>
                       fontWeight: FontWeight.w700,
                       color: const Color(0xFF454545)))),
         ]).show();
+  }
+
+  void _loadInterstitialAd() {
+    InterstitialAd.load(
+      adUnitId: "ca-app-pub-3940256099942544/8691691433",
+      request: AdRequest(),
+      adLoadCallback: InterstitialAdLoadCallback(
+        onAdLoaded: (ad) {
+          ad.fullScreenContentCallback = FullScreenContentCallback(
+            onAdDismissedFullScreenContent: (ad) {
+              print("Close Add");
+            },  
+          );
+          setState(() {
+            _isInterstitialReady = true;
+            _interstitialAd = ad;
+          });
+        },
+        onAdFailedToLoad: (err) {
+          _isInterstitialReady = false;
+          _interstitialAd.dispose();
+        })
+    );
   }
 }
